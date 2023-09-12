be6c61635e0b51fffb72ede25fb36a023585f56f
Mr Sherry <mr.sherry.793@gmail.com>
2023-09-12 19:06:01 +0500

    server render and show dofferent products

--- index.js
+++ index.js
@@ -71,11 +71,28 @@ const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const product = data.products[0];
+const products = data.products;
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  //   console.log(req.url);
+
+  if (req.url.startsWith("/product")) {
+    // console.log(req.url.split("/"));
+    const id = req.url.split("/")[2];
+
+    const product = products.find((p) => p.id === (+id));
+    console.log(product);
+
+    res.setHeader("Content-Type", "text/html");
+    const modifyIndex = index
+      .replace("**title**", product.title)
+      .replace("**url**", product.thumbnail)
+      .replace("**price**", product.price)
+      .replace("**rating**", product.rating);
+    res.end(modifyIndex);
+    return;
+  }
 
   switch (req.url) {
     case "/":
@@ -85,16 +102,6 @@ const server = http.createServer((req, res) => {
     case "/data":
       res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify(data));
-      break;
-    case "/product":
-      res.setHeader("Content-Type", "text/html");
-
-      const modifyIndex = index
-        .replace("**title**", product.title)
-        .replace("**url**", product.thumbnail)
-        .replace("**price**", product.price)
-        .replace("**rating**", product.rating);
-      res.end(modifyIndex);
       break;
 
     default:
